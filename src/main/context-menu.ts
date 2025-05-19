cd142d4090adf4d1068ff2cac4bb7fe9d219984c
sealday <sealday@gmail.com>
2025-05-19 19:50:19 +0800

    fix: lint

--- src/main/context-menu.ts
+++ src/main/context-menu.ts
@@ -6,8 +6,8 @@ import {
 } from 'electron';
 
 import { ipcMainManager } from './ipc';
-import { isDevMode } from './utils/devmode';
 import { IpcEvents } from '../ipc-events';
+import { isDevMode } from './utils/devmode';
 
 /**
  * Returns items related to running the current fiddle.
